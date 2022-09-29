74846d69f069bc28a975c84c4fb6b01cc925c373
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2022-09-29 00:09:25 -0400

    add oracle generation, obtain location info from api

--- src/main/java/org/CodeTrackerAPI/REST.java
+++ src/main/java/org/CodeTrackerAPI/REST.java
@@ -13,6 +13,7 @@ import io.undertow.server.handlers.PathHandler;
 import io.undertow.util.Headers;
 import io.undertow.util.HttpString;
 import java.io.File;
+import java.io.FileWriter;
 import java.time.*;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
@@ -49,24 +50,13 @@ public class REST {
               String commitId = params.get("commitId").getFirst();
               String filePath = params.get("filePath").getFirst();
               String name = params.get("selection").getFirst();
-              String parentMethod = null;
-              Integer parentMethodLine = 0;
-              Integer endLineNumber = 0;
               String latestCommitHash;
-
-              try {
-                parentMethod = params.get("parentMethod").getFirst();
-                parentMethodLine =
-                  Integer.parseInt(params.get("parentMethodLine").getFirst());
-                endLineNumber =
-                  Integer.parseInt(params.get("endLineNumber").getFirst());
-              } catch (Exception e) {}
 
               Integer lineNumber = Integer.parseInt(
                 params.get("lineNumber").getFirst()
               );
 
-              String response = "";
+              String changes = "";
               GitService gitService = new GitServiceImpl();
 
               try (
@@ -95,11 +85,33 @@ public class REST {
                   lineNumber
                 );
                 CodeElement codeElement = locator.locate();
+
                 if (codeElement == null) {
                   throw new Exception("Selected code element is invalid.");
                 }
+
+                String response =
+                  "{\"repositoryName\": \"" +
+                  owner +
+                  "\"," +
+                  "\"repositoryWebURL\": \"https://github.com/" +
+                  owner +
+                  "/" +
+                  repoName +
+                  ".git" +
+                  "\"," +
+                  "\"filePath\": \"" +
+                  filePath +
+                  "\"," +
+                  "\"selectionType\": \"" +
+                  codeElement.getClass().getSimpleName() +
+                  "\"," +
+                  "\"codeElementType\": \"" +
+                  codeElement.getLocation().getCodeElementType() +
+                  "\",";
+
                 if (codeElement.getClass() == Method.class) {
-                  response =
+                  changes =
                     CTMethod(
                       owner,
                       repository,
@@ -109,19 +121,27 @@ public class REST {
                       lineNumber
                     );
                 } else if (codeElement.getClass() == Variable.class) {
+                  Variable variable = (Variable) codeElement;
                   response =
+                    response +
+                    "\"functionName\": \"" +
+                    variable.getOperation().getName() +
+                    "\"," +
+                    "\"functionStartLine\": " +
+                    variable.getOperation().getLocationInfo().getStartLine() +
+                    ",";
+                  changes =
                     CTVariable(
                       owner,
                       repository,
                       filePath,
                       commitId,
-                      parentMethod,
                       name,
-                      parentMethodLine,
-                      lineNumber
+                      lineNumber,
+                      codeElement
                     );
                 } else if (codeElement.getClass() == Attribute.class) {
-                  response =
+                  changes =
                     CTAttribute(
                       owner,
                       repository,
@@ -131,19 +151,34 @@ public class REST {
                       lineNumber
                     );
                 } else if (codeElement.getClass() == Block.class) {
+                  Block block = (Block) codeElement;
                   response =
-                    CTBlock(
-                      owner,
-                      repository,
-                      filePath,
-                      commitId,
-                      parentMethod,
-                      name,
-                      parentMethodLine,
-                      lineNumber,
-                      endLineNumber
-                    );
+                    response +
+                    "\"functionName\": \"" +
+                    block.getOperation().getName() +
+                    "\"," +
+                    "\"functionStartLine\": " +
+                    block.getOperation().getLocationInfo().getStartLine() +
+                    "," +
+                    "\"blockStartLine\": " +
+                    codeElement.getLocation().getStartLine() +
+                    "," +
+                    "\"blockEndLine\": " +
+                    codeElement.getLocation().getEndLine() +
+                    ",";
+                  changes =
+                    CTBlock(owner, repository, filePath, commitId, codeElement);
                 }
+
+                response =
+                  response +
+                  "\"startCommitId\": \"" +
+                  commitId +
+                  "\"," +
+                  "\"changes\": " +
+                  changes +
+                  "}";
+
                 exchange
                   .getResponseHeaders()
                   .put(new HttpString("Access-Control-Allow-Origin"), "*")
@@ -170,7 +205,7 @@ public class REST {
               );
               String response;
               GitService gitService = new GitServiceImpl();
-              
+
               try (
                 Repository repository = gitService.cloneIfNotExists(
                   "tmp/" + repoName,
@@ -197,6 +232,7 @@ public class REST {
                   lineNumber
                 );
                 CodeElement codeElement = locator.locate();
+
                 if (codeElement == null) {
                   throw new Exception("Selected code element is invalid.");
                 }
@@ -210,6 +246,162 @@ public class REST {
                   response = "{\"type\": \"Block\"}";
                 } else {
                   response = "{\"type\": \"Invalid Element\"}";
+                }
+
+                exchange
+                  .getResponseHeaders()
+                  .put(new HttpString("Access-Control-Allow-Origin"), "*")
+                  .put(Headers.CONTENT_TYPE, "text/plain");
+                exchange.getResponseSender().send(response);
+              } catch (Exception e) {
+                System.out.println("Something went wrong: " + e);
+                exchange
+                  .getResponseHeaders()
+                  .put(new HttpString("Access-Control-Allow-Origin"), "*")
+                  .put(Headers.CONTENT_TYPE, "text/plain");
+                exchange
+                  .getResponseSender()
+                  .send("{\"type\": \"Invalid Element\"}");
+              }
+            }
+          )
+          .get(
+            "/addToOracle",
+            exchange -> {
+              Map<String, Deque<String>> params = exchange.getQueryParameters();
+              String owner = params.get("owner").getFirst();
+              String repoName = params.get("repoName").getFirst();
+              String commitId = params.get("commitId").getFirst();
+              String filePath = params.get("filePath").getFirst();
+              String name = params.get("selection").getFirst();
+              Boolean valid = Boolean.parseBoolean(
+                params.get("valid").getFirst()
+              );
+              String folderName = valid ? "true" : "false";
+              String latestCommitHash;
+              String changes = "";
+
+              Integer lineNumber = Integer.parseInt(
+                params.get("lineNumber").getFirst()
+              );
+              GitService gitService = new GitServiceImpl();
+
+              try (
+                Repository repository = gitService.cloneIfNotExists(
+                  "tmp/" + repoName,
+                  "https://github.com/" + owner + "/" + repoName + ".git"
+                )
+              ) {
+                try (Git git = new Git(repository)) {
+                  PullResult call = git.pull().call();
+                  System.out.println(
+                    "Pulled from the remote repository: " + call
+                  );
+                  latestCommitHash =
+                    git.log().setMaxCount(1).call().iterator().next().getName();
+                }
+                if ("master".equals(commitId)) {
+                  commitId = latestCommitHash;
+                }
+
+                CodeElementLocator locator = new CodeElementLocator(
+                  repository,
+                  commitId,
+                  filePath,
+                  name,
+                  lineNumber
+                );
+                CodeElement codeElement = locator.locate();
+
+                if (codeElement == null) {
+                  throw new Exception("Selected code element is invalid.");
+                }
+                if (codeElement.getClass() != Block.class) {
+                  throw new Exception("Selected code element is not a block.");
+                }
+
+                String response =
+                  "{\"repositoryName\": \"" +
+                  owner +
+                  "\"," +
+                  "\"repositoryWebURL\": \"https://github.com/" +
+                  owner +
+                  "/" +
+                  repoName +
+                  ".git" +
+                  "\"," +
+                  "\"filePath\": \"" +
+                  filePath +
+                  "\"," +
+                  "\"selectionType\": \"" +
+                  codeElement.getClass().getSimpleName() +
+                  "\"," +
+                  "\"codeElementType\": \"" +
+                  codeElement.getLocation().getCodeElementType() +
+                  "\",";
+
+                Block block = (Block) codeElement;
+                changes =
+                  CTBlock(owner, repository, filePath, commitId, codeElement);
+                response =
+                  response +
+                  "\"functionName\": \"" +
+                  block.getOperation().getName() +
+                  "\"," +
+                  "\"functionStartLine\": " +
+                  block.getOperation().getLocationInfo().getStartLine() +
+                  "," +
+                  "\"blockStartLine\": " +
+                  codeElement.getLocation().getStartLine() +
+                  "," +
+                  "\"blockEndLine\": " +
+                  codeElement.getLocation().getEndLine() +
+                  "," +
+                  "\"startCommitId\": \"" +
+                  commitId +
+                  "\"," +
+                  "\"changes\": " +
+                  changes +
+                  "}";
+
+                try {
+                  // Creates a Writer using FileWriter
+                  String fileName =
+                    "src/main/resources/oracle/block/training/" +
+                    folderName +
+                    "/" +
+                    repoName +
+                    "-" +
+                    block.getOperation().getClassName().split("\\.")[block.getOperation().getClassName().split("\\.").length -1]+
+                    "-" +
+                    block.getOperation().getName() +
+                    "-" +
+                    codeElement.getLocation().getCodeElementType();
+
+                  // check if file already exists, add numerals at the end if true
+                  if (new File(fileName + ".json").isFile()) {
+                    Integer i = 1;
+                    while (
+                      new File(fileName + "-" + i.toString() + ".json").isFile()
+                    ) {
+                      i++;
+                    }
+                    fileName = fileName + "-" + i.toString() + ".json";
+                  } else {
+                    fileName = fileName + ".json";
+                  }
+
+                  FileWriter output = new FileWriter(fileName);
+
+                  // Writes the program to file
+                  output.write(response);
+                  System.out.println("Data is written to the file: " + fileName);
+
+                  // Closes the writer
+                  output.close();
+                } catch (Exception e) {
+                  e.getStackTrace();
+                  System.out.println(e);
                 }
 
                 exchange
@@ -298,6 +490,7 @@ public class REST {
           historyInfo.getElementBefore(),
           historyInfo.getElementAfter(),
           historyInfo.getCommitterName(),
+          historyInfo.getCommitTime(),
           currentChanges,
           evolutionPresent,
           evolutionHook
@@ -313,8 +506,8 @@ public class REST {
         JsonAutoDetect.Visibility.ANY
       );
       try {
+        Collections.reverse(changeLog);
         String json = mapper.writeValueAsString(changeLog);
-        // System.out.println("JSON = " + json);
         return json;
       } catch (JsonProcessingException e) {
         e.printStackTrace();
@@ -331,20 +524,22 @@ public class REST {
     Repository repository,
     String filePath,
     String commitId,
-    String methodName,
     String variableName,
-    Integer methodLineNumber,
-    Integer variableLineNumber
+    Integer variableLineNumber,
+    CodeElement codeElement
   ) {
     ArrayList<CTHVariable> changeLog = new ArrayList<>();
+    Variable variable = (Variable) codeElement;
     try {
       VariableTracker variableTracker = CodeTracker
         .variableTracker()
         .repository(repository)
         .filePath(filePath)
         .startCommitId(commitId)
-        .methodName(methodName)
-        .methodDeclarationLineNumber(methodLineNumber)
+        .methodName(variable.getOperation().getName())
+        .methodDeclarationLineNumber(
+          variable.getOperation().getLocationInfo().getStartLine()
+        )
         .variableName(variableName)
         .variableDeclarationLineNumber(variableLineNumber)
         .build();
@@ -388,6 +583,7 @@ public class REST {
           historyInfo.getElementBefore(),
           historyInfo.getElementAfter(),
           historyInfo.getCommitterName(),
+          historyInfo.getCommitTime(),
           currentChanges,
           evolutionPresent,
           evolutionHook
@@ -404,8 +600,8 @@ public class REST {
         JsonAutoDetect.Visibility.ANY
       );
       try {
+        Collections.reverse(changeLog);
         String json = mapper.writeValueAsString(changeLog);
-        // System.out.println("JSON = " + json);
         return json;
       } catch (JsonProcessingException e) {
         e.printStackTrace();
@@ -476,6 +672,7 @@ public class REST {
           historyInfo.getElementBefore(),
           historyInfo.getElementAfter(),
           historyInfo.getCommitterName(),
+          historyInfo.getCommitTime(),
           currentChanges,
           evolutionPresent,
           evolutionHook
@@ -491,8 +688,8 @@ public class REST {
         JsonAutoDetect.Visibility.ANY
       );
       try {
+        Collections.reverse(changeLog);
         String json = mapper.writeValueAsString(changeLog);
-        // System.out.println("JSON = " + json);
         return json;
       } catch (JsonProcessingException e) {
         e.printStackTrace();
@@ -509,35 +706,23 @@ public class REST {
     Repository repository,
     String filePath,
     String commitId,
-    String methodName,
-    String blockName,
-    Integer methodLineNumber,
-    Integer blockStartLineNumber,
-    Integer blockEndLineNumber
+    CodeElement codeElement
   ) {
     ArrayList<CTHBlock> changeLog = new ArrayList<>();
     try {
-
-      LocationInfo.CodeElementType blockType = null;
-
-      if (blockName.equals("for")) {
-        blockType = LocationInfo.CodeElementType.FOR_STATEMENT;
-      } else if (blockName.equals("try")) {
-        blockType = LocationInfo.CodeElementType.TRY_STATEMENT;
-      } else if (blockName.equals("if")) {
-        blockType = LocationInfo.CodeElementType.IF_STATEMENT;
-      }
-
+      Block block = (Block) codeElement;
       BlockTracker blockTracker = CodeTracker
         .blockTracker()
         .repository(repository)
         .filePath(filePath)
         .startCommitId(commitId)
-        .methodName(methodName)
-        .methodDeclarationLineNumber(methodLineNumber)
-        .codeElementType(blockType)
-        .blockStartLineNumber(blockStartLineNumber)
-        .blockEndLineNumber(blockEndLineNumber)
+        .methodName(block.getOperation().getName())
+        .methodDeclarationLineNumber(
+          block.getOperation().getLocationInfo().getStartLine()
+        )
+        .codeElementType(codeElement.getLocation().getCodeElementType())
+        .blockStartLineNumber(codeElement.getLocation().getStartLine())
+        .blockEndLineNumber(codeElement.getLocation().getEndLine())
         .build();
 
       History<Block> blockHistory = blockTracker.track();
@@ -579,6 +764,7 @@ public class REST {
           historyInfo.getElementBefore(),
           historyInfo.getElementAfter(),
           historyInfo.getCommitterName(),
+          historyInfo.getCommitTime(),
           currentChanges,
           evolutionPresent,
           evolutionHook
@@ -595,8 +781,8 @@ public class REST {
         JsonAutoDetect.Visibility.ANY
       );
       try {
+        Collections.reverse(changeLog);
         String json = mapper.writeValueAsString(changeLog);
-        // System.out.println("JSON = " + json);
         return json;
       } catch (JsonProcessingException e) {
         e.printStackTrace();
@@ -621,6 +807,7 @@ public class REST {
     Integer afterLine;
     String afterPath;
     String committer;
+    Long commitTime;
     ArrayList<String> changes;
     String evolutionHook;
     Integer evolutionHookLine;
@@ -633,6 +820,7 @@ public class REST {
       CodeElement before,
       CodeElement after,
       String committer,
+      Long commitTime,
       ArrayList<String> changes,
       Boolean evolutionPresent,
       CodeElement evolutionHook
@@ -671,6 +859,7 @@ public class REST {
     Integer afterLine;
     String afterPath;
     String committer;
+    Long commitTime;
     ArrayList<String> changes;
     String evolutionHook;
     Integer evolutionHookLine;
@@ -683,6 +872,7 @@ public class REST {
       CodeElement before,
       CodeElement after,
       String committer,
+      Long commitTime,
       ArrayList<String> changes,
       Boolean evolutionPresent,
       CodeElement evolutionHook
@@ -722,6 +912,7 @@ public class REST {
     Integer afterLine;
     String afterPath;
     String committer;
+    Long commitTime;
     ArrayList<String> changes;
     String evolutionHook;
     Integer evolutionHookLine;
@@ -734,6 +925,7 @@ public class REST {
       CodeElement before,
       CodeElement after,
       String committer,
+      Long commitTime,
       ArrayList<String> changes,
       Boolean evolutionPresent,
       CodeElement evolutionHook
@@ -772,6 +964,7 @@ public class REST {
     Integer afterLine;
     String afterPath;
     String committer;
+    Long commitTime;
     ArrayList<String> changes;
     String evolutionHook;
     Integer evolutionHookLine;
@@ -784,6 +977,7 @@ public class REST {
       CodeElement before,
       CodeElement after,
       String committer,
+      Long commitTime,
       ArrayList<String> changes,
       Boolean evolutionPresent,
       CodeElement evolutionHook
@@ -801,6 +995,7 @@ public class REST {
       System.out.println("ID: " + after.getIdentifierIgnoringVersion());
 
       this.committer = committer;
+      this.commitTime = commitTime;
       this.changes = changes;
 
       if (evolutionPresent) {
